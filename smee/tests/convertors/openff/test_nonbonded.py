82b95096897bd535042d692bf048ac25e450439d
Lily Wang <lily@mdanalysis.org>
2026-02-17 18:55:00 +1100

    updates

--- smee/tests/convertors/openff/test_nonbonded.py
+++ smee/tests/convertors/openff/test_nonbonded.py
@@ -96,6 +96,7 @@ def test_convert_electrostatics_v_site(toolkit_registry_rdkit_first):
     interchange = force_field.create_interchange(
         molecule.to_topology(),
         toolkit_registry=toolkit_registry_rdkit_first,
+        allow_nonintegral_charges=True
     )
     charge_collection = interchange.collections["Electrostatics"]
 
@@ -180,6 +181,7 @@ def test_convert_electrostatics_tip4p(toolkit_registry_rdkit_first):
     interchange = force_field.create_interchange(
         molecule.to_topology(),
         toolkit_registry=toolkit_registry_rdkit_first,
+        allow_nonintegral_charges=True
     )
 
     tensor_top: smee.TensorTopology
@@ -224,7 +226,7 @@ def test_convert_bci_and_vsite(toolkit_registry_rdkit_first):
     mol.assign_partial_charges(charge_handler.partial_charge_method)
 
     interchange = ff_off.create_interchange(
-        mol.to_topology(), toolkit_registry=toolkit_registry_rdkit_first
+        mol.to_topology(), toolkit_registry=toolkit_registry_rdkit_first,
     )
 
     expected_charges = [
@@ -312,7 +314,7 @@ def test_convert_dexp(ethanol, test_data_dir, toolkit_registry_rdkit_first):
     )
 
     interchange = ff.create_interchange(
-        ethanol.to_topology(), toolkit_registry=toolkit_registry_rdkit_first
+        ethanol.to_topology(), toolkit_registry=toolkit_registry_rdkit_first,
     )
     vdw_collection = interchange.collections["DoubleExponential"]
 
